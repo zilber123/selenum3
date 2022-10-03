c037c01a8b125efe2c764c7fa70753c9c684402e
morad <morad.602@hotmail.com>
2022-10-03 20:39:36 +0300

    selenumFinal

---
+++ src/test/java/Helper.java
@@ -0,0 +1,42 @@
+import org.bouncycastle.pqc.crypto.newhope.NHOtherInfoGenerator;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
+public class Helper {
+
+     public static final String CHROMEKEY = "webdriver.chrome.driver";
+     public static final String CHROMEPATH = "res\\chromedriver.exe";
+     public static final String WEB ="https://automationpanda.com/2021/12/29/want-to-practice-test-automation-try-these-demo-sites/";;
+     public static final String BOOKS ="//*[@id=\"menu-item-9165\"]/a";
+     public static final String HOME ="//*[@id=\"menu-item-8\"]/a";
+     public static final String ABOUT ="//*[@id=\"menu-item-9\"]/a";
+     public static final String FOLLOW ="//*[@id=\"subscribe-submit\"]/button";
+
+     public static final String Development ="//*[@id=\"menu-item-6832\"]/a";
+
+     public static final String EMAIL ="public static final String";
+     public static final String login ="//*[@id=\"features\"]/div/div[3]/a";
+     public static final String projectScreenshots ="https://www.istockphoto.com/photo/happy-birthday-delicious-cupcake-with-burning-candle-on-light-background-gm1327255314-411664842?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=http%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fhappy%2520birthday%2F&utm_term=happy+birthday";
+
+     public static final String TXT = ".txt";
+     public static final String WIKI= "https://en.wikipedia.org/wiki/Infected_Mushroom";
+     public static final String WIKI2 ="//*[@id=\"mw-content-text\"]/div[1]/p[2]";
+     public static final String NAME = "WIKI TXT";
+
+     public static final String FOLDER = "C:\\Users\\morad\\Documents\\";
+
+
+     public static WebDriver Object;
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/Homework.java
@@ -0,0 +1,134 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.Keys.CONTROL;
+import static org.openqa.selenium.Keys.TAB;
+public class Homework {
+    private Object args;
+
+
+    @Test
+    public void openBrowser() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        Thread.sleep(5000);
+        driver.get(Helper.WEB);
+
+
+        driver.manage().window().fullscreen();
+        WebDriverWait wait = new WebDriverWait(driver, ofSeconds(10));
+        Thread.sleep(400);
+        WebElement loginField = driver.findElement(By.xpath(Helper.BOOKS));
+        Thread.sleep(400);
+        loginField.click();
+//switch with the help of actions class
+        Actions action = new Actions(driver);
+        action.keyDown(CONTROL).sendKeys(TAB).build().perform(); //opening the URL saved.
+
+        WebElement loginField1 = driver.findElement(By.xpath(Helper.HOME));
+        driver.manage().window().maximize();
+        Thread.sleep(600);
+        loginField1.click();
+        driver.manage().window().maximize();
+        WebElement loginField2 = driver.findElement(By.xpath(Helper.ABOUT));
+        Thread.sleep(600);
+        loginField2.click();
+        driver.manage().window().maximize();
+        WebElement loginField3 = driver.findElement(By.xpath(Helper.FOLLOW));
+        Thread.sleep(600);
+        loginField3.click();
+        WebElement loginField4 = driver.findElement(By.xpath(Helper.Development));
+        loginField4.click();
+
+
+//Open a new tab using
+        ArrayList<String> tab = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tab.get(3));
+        Thread.sleep(600);
+
+
+//        ArrayList<String> tab1 = new ArrayList<>(driver.getWindowHandles());
+//        driver.switchTo().window(tab.get(1));
+//        Thread.sleep(600);
+//        driver.close();
+        //
+
+        //  screenshot
+        {
+
+
+            //navigate to url
+            driver.get(Helper.projectScreenshots);
+            Thread.sleep(600);
+            //Take the screenshot
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            Thread.sleep(600);
+            //Copy the file to a location and use try catch block to handle exception
+            try {
+                FileUtils.copyFile(screenshot, new File("C:\\projectScreenshots\\homePageScreenshot.png"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+        driver.close();
+    }
+
+
+    // 6
+
+
+    @Test
+    public static void createFolder(String folderName, WebDriver driver) throws IOException {
+        driver.manage().window().maximize();
+        driver.get(Helper.WIKI);
+        WebDriverWait wait = new WebDriverWait(driver, ofSeconds(4));
+        String worksheets = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(Helper.WIKI2))).getText();
+        File file = new File(Helper.FOLDER + Helper.TXT);
+        FileWriter writer = new FileWriter(file);
+        writer.write(worksheets);
+        writer.close();
+    }
+
+    public static void createFile(String pathname, String filename, String text) {
+        File file = new File(pathname + filename);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("file was created");
+            } else {
+                System.out.println("file already exists");
+            }
+            FileWriter writer = new FileWriter(file);
+            writer.write(text);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
