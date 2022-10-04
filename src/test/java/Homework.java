83a1119ba2e1d98df72e63a43d5115e018548392
mordechai 90 <112845546+zilber123@users.noreply.github.com>
2022-10-04 12:45:23 +0300

    Update Homework.java

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -7,13 +7,10 @@ import org.testng.annotations.Test;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.ArrayList;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
-
 import static java.time.Duration.ofSeconds;
 import static org.openqa.selenium.Keys.CONTROL;
 import static org.openqa.selenium.Keys.TAB;
@@ -34,7 +31,7 @@ public class Homework {
         WebElement loginField = driver.findElement(By.xpath(Helper.BOOKS));
         Thread.sleep(400);
         loginField.click();
-//switch with the help of actions class
+        //switch with the help of actions class
         Actions action = new Actions(driver);
         action.keyDown(CONTROL).sendKeys(TAB).build().perform(); //opening the URL saved.
 
@@ -44,54 +41,57 @@ public class Homework {
         loginField1.click();
         driver.manage().window().maximize();
         WebElement loginField2 = driver.findElement(By.xpath(Helper.ABOUT));
+
         Thread.sleep(600);
         loginField2.click();
         driver.manage().window().maximize();
         WebElement loginField3 = driver.findElement(By.xpath(Helper.FOLLOW));
+
         Thread.sleep(600);
         loginField3.click();
         WebElement loginField4 = driver.findElement(By.xpath(Helper.Development));
         loginField4.click();
 
 
-//Open a new tab using
-        ArrayList<String> tab = new ArrayList<>(driver.getWindowHandles());
-        driver.switchTo().window(tab.get(3));
-        Thread.sleep(600);
 
-
-//        ArrayList<String> tab1 = new ArrayList<>(driver.getWindowHandles());
-//        driver.switchTo().window(tab.get(1));
-//        Thread.sleep(600);
-//        driver.close();
-        //
-
-        //  screenshot
-        {
-
-
-            //navigate to url
-            driver.get(Helper.projectScreenshots);
+            ArrayList<String> tab = new ArrayList<>(driver.getWindowHandles());
+            driver.switchTo().window(tab.get(3));
             Thread.sleep(600);
-            //Take the screenshot
-            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-            Thread.sleep(600);
-            //Copy the file to a location and use try catch block to handle exception
-            try {
-                FileUtils.copyFile(screenshot, new File("C:\\projectScreenshots\\homePageScreenshot.png"));
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            }
-
+            {
+                driver.get(Helper.projectScreenshots);
+                Thread.sleep(600);
+                //Take the screenshot
+                File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                Thread.sleep(600);
+                //Copy the file to a location and use try catch block to handle exception
+                try {
+                    FileUtils.copyFile(screenshot, new File("C:\\projectScreenshots\\homePageScreenshot.png"));
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }driver.close();
         }
-        driver.close();
+
     }
 
+//test2
+            @Test
+            public void openBrowser2() throws InterruptedException {
+                WebDriver driver = new ChromeDriver();
+                Thread.sleep(800);
+                driver.get(Helper.WEB2);
+                driver.manage().window().fullscreen();
+                Thread.sleep(800);
+                driver.manage().window().maximize();
+                WebElement loginField1 = driver.findElement(By.xpath(Helper.CLICK2));
+                loginField1.sendKeys(Helper.TAP );
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("window.scrollBy(0,250)", "300");
+                Thread.sleep(800);
+                WebElement loginField2 = driver.findElement(By.xpath(Helper.play));
+                loginField2.click();
+            }
 
-    // 6
-
-
-    @Test
+     @Test
     public static void createFolder(String folderName, WebDriver driver) throws IOException {
         driver.manage().window().maximize();
         driver.get(Helper.WIKI);
@@ -102,7 +102,7 @@ public class Homework {
         writer.write(worksheets);
         writer.close();
     }
-
+    @Test
     public static void createFile(String pathname, String filename, String text) {
         File file = new File(pathname + filename);
         try {
@@ -120,9 +120,18 @@ public class Homework {
     }
 
 
+}
 
 
-    }
+
+
+
+
+
+
+
+
+
 
 
 
